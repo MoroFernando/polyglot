ddd89ad0689dc39994a0e221d62c5684ba73abe3
Fernando Moro <Morofernando99@gmail.com>
2024-05-04 18:05:16 -0300

    incluido system prompt separado do input do usario

--- utils/gemini/generateText.jsx
+++ utils/gemini/generateText.jsx
@@ -1,12 +1,60 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const MODEL_NAME = "gemini-1.5-pro-latest";
+const API_KEY = process.env.GEMINI_API_KEY;
 
-async function generateText(prompt) {
 
-  const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
+async function generateText(prompt, systemPrompt, responseJSON) {
+
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ 
+    model: MODEL_NAME,
+    systemInstruction: {
+      parts: [
+        {text: `${systemPrompt}`},
+      ],
+    },
+  });
+
+  const generationConfig = {
+    temperature: 1,
+    topK: 0,
+    topP: 0.95,
+    maxOutputTokens: 8192,
+  };
+
+  if(responseJSON){
+    generationConfig.response_mime_type = "application/json"
+  }
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
   const text = response.text();
   return text
 }
